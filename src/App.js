3c6e6ab7f64814bff57cdf0aa84f00dba1a2c185
Vangmay Sachan <vangmay.sachan16@gmail.com>
2024-05-14 16:39:49 +0530

    Add ex0

--- src/App.js
+++ src/App.js
@@ -3,42 +3,7 @@ import "./App.css";
 function App() {
   return (
     <>
-      <header>
-        <h1>Orbital todos</h1>
-        <h2>Welcome back Name</h2>
-        <button>Change name</button>
-      </header>
-      <main>
-        <h2>Add new tasks</h2>
-        <form>
-          <input />
-          <button type="submit">Add task</button>
-        </form>
-
-        <table>
-          <thead>
-            <tr>
-              <th>No.</th>
-              <th>Task</th>
-              <th>Completed?</th>
-            </tr>
-          </thead>
-        </table>
-        <tbody>
-          <tr>
-            <td>Wash dishes</td>
-            <input type="checkbox" />
-          </tr>
-          <tr>
-            <td>Take a shower</td>
-            <input type="checkbox" />
-          </tr>
-          <tr>
-            <td>Do laundry</td>
-            <input type="checkbox" />
-          </tr>
-        </tbody>
-      </main>
+      <h1>Hello world</h1>
     </>
   );
 }
